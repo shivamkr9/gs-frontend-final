26abbd79cc9ae178808dbebb1a77885e45c76f31
theKnightOPS <sk9355622@gmail.com>
2023-10-18 22:11:16 +0530

    Uploaded code

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,4 +1,11 @@
 import Navigation from "@/components/Navigation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Profile Page',
+    description: 'Test Meta Data',
+}
+
 
 export default function Page() {
     return (
